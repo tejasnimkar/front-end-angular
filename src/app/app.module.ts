4e71ae8ef28ae3fe7bb77f61c701c31226f70cea
tejas <tjs.nimkar@gmail.com>
2020-08-24 21:22:30 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+import { Subscription } from 'rxjs';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatListModule } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +23,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    FlexLayoutModule,
+    LayoutModule,
+    MatListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
