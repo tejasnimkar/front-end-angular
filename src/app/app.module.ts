095d5b44c07739deb7844776ebae1fa89215cb28
tejas <tjs.nimkar@gmail.com>
2020-08-26 01:20:08 +0530

    components-seprated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-
+import { MatDividerModule } from '@angular/material/divider';
 import { Subscription } from 'rxjs';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatListModule } from '@angular/material/list';
@@ -19,10 +19,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    SideBarComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +45,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatListModule,
     MatExpansionModule,
     MatCardModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatDividerModule
 
   ],
   providers: [],
